fb93e6901a72136d65fd19f2dce69c85d903b56e
Will Green <will.green98@hotmail.com>
2021-07-01 07:15:59 +0100

    Main.py init
    Upload python started - track back to empty file if needs be

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+__author__ = "Will Green - UP853829"
+__copyright__ = "Copyright 2021, @ME"
+__credits__ = ["Will Green", ""]
+__license__ = "GPL"
+__version__ = "0.1"
+__maintainer__ = "Will green"
+__email__ = "up853829@myport.ac.uk"
+__status__ = "Testing"
+
+if __name__ == '__main__':
+    print("PJE Code")
